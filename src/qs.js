3476c658539fcb0d999fc9ee7469060dacf1443c
Boris Okunskiy <i@savant.pro>
2015-05-28 16:39:17 +0300

    create -> replace; add, remove, removeAll

--- src/qs.js
+++ src/qs.js
@@ -1,26 +1,59 @@
 'use strict';
 
 var url = require('url')
-  , querystring = require('querystring');
+  , querystring = require('querystring')
+  , _ = require('underscore');
 
 module.exports = exports = function (req) {
 
   var qs = querystring.parse(url.parse(req.originalUrl).query);
 
   return {
+
     get: function (key) {
       return qs[key];
     },
-    create: function (obj) {
+
+    _parse: function () {
       var r = {};
       Object.keys(qs).forEach(function (key) {
-        r[key] = qs[key];
+        r[key] = toArray(qs[key]);
       });
+      return r;
+    },
+
+    replace: function (obj) {
+      var r = this._parse();
       Object.keys(obj).forEach(function (key) {
         r[key] = obj[key];
       });
       return querystring.stringify(r);
     },
+
+    add: function (obj) {
+      var r = this._parse();
+      Object.keys(obj).forEach(function (key) {
+        r[key] = _.union(r[key], toArray(obj[key]));
+      }, this);
+      return querystring.stringify(r);
+    },
+
+    remove: function (obj) {
+      var r = this._parse();
+      Object.keys(obj).forEach(function (key) {
+        r[key] = _.difference(r[key], toArray(obj[key]));
+      });
+      return querystring.stringify(r);
+    },
+
+    removeAll: function (keys) {
+      var r = this._parse();
+      Object.keys(keys).forEach(function (key) {
+        delete r[key];
+      });
+      return querystring.stringify(r);
+    },
+
     is: function () {
       var argv = [].slice.apply(arguments);
       var name = argv[0]
@@ -32,3 +65,7 @@ module.exports = exports = function (req) {
   };
 
 };
+
+function toArray(value) {
+  return Array.isArray(value) ? value : [value];
+}
